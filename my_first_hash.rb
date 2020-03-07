4f64ab9407788dbbf1dcd9aaae30c3627ef492e1
Terrence Bell <terrencekbell@gmail.com>
2020-03-07 01:48:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,7 +33,7 @@ def adding
                  }
                  shipping_manifest["muskets"] = 2 
                  shipping_manifest["gun powder"] = 4
-                 shipping_manifest
+                 return shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
